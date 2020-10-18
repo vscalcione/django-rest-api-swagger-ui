25e2e20c1657b56fe88e5b0c67713249d2e03199
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-18 21:18:14 +0200

    Initial commit

---
+++ userapi/userapi_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserapiAppConfig(AppConfig):
+    name = 'userapi_app'
---
+++ userapi/userapi_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+	age = models.IntegerField()
+
+	def __str__(self):
+		return self.first_name
+
+
